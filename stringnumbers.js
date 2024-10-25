63ad3a2d2d9f1b6915fd9fdc8aae3bd6defe3b7d
Aman Dhingra <amandhingra2336@gmail.com>
2024-10-25 17:21:13 +0530

    string and  numbers

---
+++ stringnumbers.js
@@ -0,0 +1,14 @@
+// let val = 3;
+// let negval = -val;
+// // console.log(val, negval);
+
+// let str1 = "hello";
+// let str2 = "world";
+
+// console.log(str1 + str2);
+console.log("1" + 2); // here number converts into a string -> "1" + "2" = "12"
+console.log("1" + 2 + 2); // -> 122
+console.log(1 + 2 + "2"); // -> 32 confusing -> es6 js primitive type conversion
+
+let counter = 1000;
+console.log(++counter);
