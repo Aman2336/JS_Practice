b2aa015f1433e1dcd73c71caaf533975874c5a8a
Aman Dhingra <amandhingra2336@gmail.com>
2024-10-25 17:11:55 +0530

    data types

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log("hello");
+// let score = true;
+// console.log(score, typeof score);
+// let val = Number(score);
+// console.log(val, typeof val);
+
+//"33" -> 33
+//"33ahvdhsb" -> NaN
+//boolean -> convert
+
+let flag = 1;
+let newflag = Boolean(flag);
+console.log(flag, typeof flag);
+console.log(newflag, typeof newflag);
+
+// 1-> true
+//""-> false
+//"fvhjjm" -> true
